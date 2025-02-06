77f155eec3dcacf3c7b3e4d0d3055f5eeb26442f
RITURAJKUMAR <riturajkumar1105@gmail.com>
2025-02-06 23:10:19 +0530

    Music Player

--- MusicPlayer/urls.py
+++ MusicPlayer/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from MusicPlayer.views import *
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+from django.conf import settings
+
 
 
 app_name = "MusicPlayer"
